0f986f2688b82d4f62d3b0987ab9b4228f0bd9bf
Kwadrat <81324444+KwadratProjects@users.noreply.github.com>
2022-05-25 12:05:16 +0200

    file upload
    uploaded files

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("I'm living inside your console")
+
+/* const item = document.getElementById('parse');
+if(item) {
+    item.onclick = () => window.location.href = "https://google.pl"
+} */
